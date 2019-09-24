1c865d1e7ba300a7c5cee1ef6c9cc2030b3edf0b
paulg <paulg@docker.lan>
2019-09-24 21:12:02 +0000

    use module

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "aws_ecs_cluster" "demo-ecs-cluster" {
+/* resource "aws_ecs_cluster" "demo-ecs-cluster" {
   name = var.tf-mn-ecs-clustername
 }
+*/ 
 
+module "ecs-cluster" {
+  source       = "springload/ecs-cluster/aws"
+  version      = "0.0.4"
+  cluster_name = var.tf-mn-ecs-clustername
+}
